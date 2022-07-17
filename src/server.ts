1942995b4f3c54bcd643c5220d4e20097dacc5ed
gercamjr <gercamjr.dev@gmail.com>
2022-07-17 08:16:26 -0500

    defined schema and server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 import express from 'express'
 import {ApolloServer, gql} from 'apollo-server-express'
 
+import {schema} from './schema'
+
+const app = express()
+const server = new ApolloServer({schema,})
+server.applyMiddleware({app})
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
